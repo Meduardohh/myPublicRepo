546a37f660b57c5bbcc8fa0f19a2d5305eeff30a
meDi <molnar.medard31@gmail.com>
2021-12-03 11:12:52 +0100

    asd

---
+++ asd/Medard_Molnar/script.js
@@ -0,0 +1,57 @@
+// function szorzoTabla(n){
+//     let eredmeny
+//     for(let i=0;i<=10;i++){
+//         eredmeny=n*i
+//         console.log(`${n}*${i} = ${eredmeny}`)
+//     }
+// }
+
+// szorzoTabla(2)
+
+
+// function stringReverse(string){
+//     if(typeof string !=='string'){
+//       return  alert("Ne buzgálkodjál. Mindig buzgálkodsz")
+        
+//     }
+//     else{
+//   let reverseString=  string.split("").reverse().join("")
+
+// return console.log(reverseString);
+// }
+// }
+
+// stringReverse("asdert")
+
+
+// function toString(number){
+//   return console.log(number.toString())
+// }
+// toString(1231827)
+
+// function increase(n)
+// {
+//   return console.log(++n)
+// }
+// increase(4)
+
+// function decrease(n)
+// {
+//   return console.log(--n)
+// }
+// decrease(4)
+
+// function add(a,b){
+//   return console.log(a+b)
+// }
+
+// add(10,20)
+
+// function subtract(a,b){
+//   return console.log(a-b)
+
+
+// }
+// subtract(10,15)
+
+
